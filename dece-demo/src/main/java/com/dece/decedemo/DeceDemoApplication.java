26aee94f2bdd8cba6143235e66504f25d67661e0
Andrew Haman <andrew.haman@yahoo.com>
2019-03-04 15:55:13 -0600

    Add files via upload

---
+++ dece-demo/src/main/java/com/dece/decedemo/DeceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dece.decedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeceDemoApplication.class, args);
+	}
+
+}
